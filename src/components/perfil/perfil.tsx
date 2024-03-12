cca92ecc256189ed0df067c8a098a2e82a2eec61
Ryan Victor <ryanwvxx@gmail.com>
2024-03-12 15:42:52 -0300

    curriculo 12/03

--- src/components/perfil/perfil.tsx
+++ src/components/perfil/perfil.tsx
@@ -40,7 +40,7 @@ const Perfil = () => {
                 </p>
 
                 <a href="https://drive.google.com/file/d/1x7F9cqXH1sIVrjHQdLD59yG7n8jhNAkM/view?usp=drive_link"
-                    download='https://drive.google.com/file/d/1x7F9cqXH1sIVrjHQdLD59yG7n8jhNAkM/view?usp=drive_link' className="link-curriculo">
+                    download='https://drive.google.com/file/d/1x7F9cqXH1sIVrjHQdLD59yG7n8jhNAkM/view?usp=drive_link' className="link-curriculo" target="_blank">
                     Meu currículo
                 </a>
             </div>
