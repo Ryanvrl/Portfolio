9fa4e6586b63c80e1a087e07a29e81dca6fcc5fc
Ryan Victor <ryanwvxx@gmail.com>
2024-03-12 16:48:02 -0300

    style 12/03

--- src/components/projetos/projetos.tsx
+++ src/components/projetos/projetos.tsx
@@ -32,10 +32,14 @@ const Projetos = () => {
       <div className="cards-projetos">
 
         <div className="card-projeto">
-          <h3>Countries API</h3>
-          <img src="./images/countries.png" alt="imagem projeto countries" className="imagem-projeto" />
-          <div className="card-infos">
-            Esse é um projeto de API que busca países e suas informações, filtrando-os pelo nome e alternando o tema, usando Html, Css, React e JavaScript.
+          <div className="container-card-filho-projeto">
+            <img src="./images/countries.png" alt="imagem projeto countries" className="imagem-projeto" />
+            <div className="card-infos">
+              <h3>Countries API</h3>
+              <p>
+                Esse é um projeto de API que busca países e suas informações, filtrando-os pelo nome e alternando o tema, usando Html, Css, React e JavaScript.
+              </p>
+            </div>
           </div>
           <div className="links-github-e-projeto">
             <a href="https://github.com/Ryanvrl/country-react" target="_blank" className="link-do-card link-github">
@@ -50,10 +54,14 @@ const Projetos = () => {
         </div>
 
         <div className="card-projeto">
-          <h3>Pokédex</h3>
-          <img src="./images/print-pokedex.png" alt="imagem projeto github" className="imagem-projeto" />
-          <div className="card-infos">
-            Faz um busca de pokemons pela PokéAPI, filtrando por nome e alternando tema usando Html, Css, React e JavaScript.
+          <div className="container-card-filho-projeto">
+            <img src="./images/print-pokedex.png" alt="imagem projeto github" className="imagem-projeto" />
+            <div className="card-infos">
+              <h3>Pokédex</h3>
+              <p>
+                Faz um busca de pokemons pela PokéAPI, filtrando por nome e alternando tema usando Html, Css, React e JavaScript.
+              </p>
+            </div>
           </div>
           <div className="links-github-e-projeto">
             <a href="https://github.com/Ryanvrl/pokedex-react" target="_blank" className="link-do-card link-github">
@@ -68,10 +76,14 @@ const Projetos = () => {
         </div>
 
         <div className="card-projeto">
-          <h3>GitHub API</h3>
-          <img src="./images/githubapi.png" alt="imagem projeto github" className="imagem-projeto" />
-          <div className="card-infos">
-            Um projeto de busca de usuários pela API do GitHub usando Html, Css, React e TypeScript.
+          <div className="container-card-filho-projeto">
+            <img src="./images/githubapi.png" alt="imagem projeto github" className="imagem-projeto" />
+            <div className="card-infos">
+              <h3>GitHub API</h3>
+              <p>
+                Um projeto de busca de usuários pela API do GitHub usando Html, Css, React e TypeScript.
+              </p>
+            </div>
           </div>
           <div className="links-github-e-projeto">
             <a href="https://github.com/Ryanvrl/gitHubApi-ts" target="_blank" className="link-do-card link-github">
@@ -86,10 +98,14 @@ const Projetos = () => {
         </div>
 
         <div className="card-projeto">
-          <h3>Portfólio</h3>
-          <img src="./images/portfolio.png" alt="imagem projeto github" className="imagem-projeto" />
-          <div className="card-infos">
-            Projeto do meu Portfólio que está sendo acessado por você nesse exato momento.
+          <div className="container-card-filho-projeto">
+            <img src="./images/portfolio.png" alt="imagem projeto github" className="imagem-projeto" />
+            <div className="card-infos">
+              <h3>Portfólio</h3>
+              <p>
+                Projeto do meu Portfólio que está sendo acessado por você nesse exato momento.
+              </p>
+            </div>
           </div>
           <div className="links-github-e-projeto">
             <a href="https://github.com/Ryanvrl/portfolio" target="_blank" className="link-do-card link-github">
@@ -106,10 +122,14 @@ const Projetos = () => {
 
 
         <div className="card-projeto">
-          <h3>Landing page</h3>
-          <img src="./images/landing-page.png" alt="imagem projeto github" className="imagem-projeto" />
-          <div className="card-infos">
-            Um projeto de busca de usuários pela API do GitHub usando Html, Css, React e TypeScript.
+          <div className="container-card-filho-projeto">
+            <img src="./images/landing-page.png" alt="imagem projeto github" className="imagem-projeto" />
+            <div className="card-infos">
+              <h3>Landing page</h3>
+              <p>
+                Um projeto de uma landing page que usa Html e css.
+              </p>
+            </div>
           </div>
           <div className="links-github-e-projeto">
             <a href="https://github.com/Ryanvrl/landing-page" target="_blank" className="link-do-card link-github">
